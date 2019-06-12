36868a47b90c72929b998e1113e9cb66ac5fa03e
Paul Trin-Dinh <paultrindinh@icloud.com>
2019-06-12 16:48:57 +0200

    English cv working, changed french cv route name for it to work

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'strangers/show'
   root 'home#index'
   match 'englishcv', to: 'home#englishcv', as: 'englishcv', via: :get
-  match 'frenchcv', to: 'home#frenchcv', as: 'frenchcv', via: :get
+  match 'francaiscv', to: 'home#francaiscv', as: 'francaiscv', via: :get
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
