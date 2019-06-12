416bfaf716101d2849b8e2b0e40c916d4d5b7c98
Paul Trin-Dinh <paultrindinh@icloud.com>
2019-06-12 17:24:27 +0200

    Changed frenchcv to francaiscv in home controller to load the pdf file correctly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     send_file 'public/documents/englishcv.pdf', type: 'application/pdf', :disposition => 'inline'
   end
 
-  def frenchcv
+  def francaiscv
     send_file 'public/documents/francaiscv.pdf', type: 'application/pdf', :disposition => 'inline'
   end
 
